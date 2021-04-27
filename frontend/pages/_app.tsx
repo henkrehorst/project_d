76e30a0dd4d72542946301940b26d36e2c71b9e2
henkrehorst <henkrehorst@outlook.com>
2021-04-27 14:39:15 +0200

    Add typescript support

---
+++ frontend/pages/_app.tsx
@@ -0,0 +1,8 @@
+import type {AppProps} from "next/app";
+
+function ProjectFrontend({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />
+}
+
+export default  ProjectFrontend;
+
---
+++ frontend/pages/index.tsx
@@ -0,0 +1,11 @@
+import {NextPage} from "next";
+
+interface PageProps {}
+
+const Page: NextPage<PageProps> = () => {
+    return (
+        <h1>Hello world!</h1>
+    )
+}
+
+export default Page;
