d1d405469b8ffe722aa95790070c87ed7e604099
markhof010 <45596281+markhof010@users.noreply.github.com>
2021-05-24 12:43:01 +0200

    Api changes

--- backend/API/API.py
+++ backend/API/API.py
@@ -4,11 +4,16 @@ from flask_restful import Api, Resource
 app = Flask(__name__)
 api = Api(app)
 
-class Testing(Resource):
-    def get(self):
-        return "Hello World!"
+class Algorithm(Resource):
+    def get(self,filename,coordinates):
+        XCoordinate1 = float(coordinates.split('-')[0].split('_')[0])
+        YCoordinate1 = float(coordinates.split('-')[0].split('_')[1])
+        XCoordinate2 = float(coordinates.split('-')[1].split('_')[0])
+        YCoordinate2 = float(coordinates.split('-')[1].split('_')[1])
 
-api.add_resource(Testing, "/testing")
+        return {"Filename": filename, "Square": makeSquare(XCoordinate1,YCoordinate1,XCoordinate2,YCoordinate2)}
+
+api.add_resource(Algorithm, "/algorithm")
 
 if __name__ == "__main__":
     app.run(debug=True)
