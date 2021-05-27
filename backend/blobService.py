414fa10eb57151d1a41424c2bbc992dba3944853
markhof010 <45596281+markhof010@users.noreply.github.com>
2021-05-27 15:06:03 +0200

    Made the blob upload in python for the images

---
+++ backend/blobService.py
@@ -0,0 +1,25 @@
+import os
+from azure.storage.blob import ContainerClient, BlobClient, ContentSettings
+
+
+def Upload(filename):
+    azure_storage_connectionstring="DefaultEndpointsProtocol=https;AccountName=projectdimages;AccountKey=h30l1NRl/ROT4XqV6YEI+RCNl6MWzLPhjswBF5pl/pOFSF3sbsaW9qaIUqQxaDbgEAoEEfciErSaJBl9TFofFQ==;EndpointSuffix=core.windows.net"
+    container_name = "images"
+    source_folder = "./data/" #Where the images are stored
+
+    my_content_settings = ContentSettings(content_type='image/png')
+
+    try:
+        client_container = ContainerClient.from_connection_string(conn_str=azure_storage_connectionstring,container_name=container_name)
+        blob = client_container.get_blob_client(filename)
+        if client_container.exists():
+            blob.delete_blob()
+
+        blob_client = BlobClient.from_connection_string(conn_str=azure_storage_connectionstring,container_name=container_name,blob_name=filename)
+        
+
+        with open(source_folder + filename,"rb") as data:
+            blob_client.upload_blob(data,content_settings=my_content_settings)
+
+    except Exception as e:
+        print(e)
