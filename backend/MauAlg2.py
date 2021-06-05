c68e676786e7290322d7fd0e1b9f19b4057d2522
mauricebunk <mauricebunk@users.noreply.github.com>
2021-06-05 13:21:32 +0200

    yoyoyo

--- backend/MauAlg2.py
+++ backend/MauAlg2.py
@@ -1,10 +1,13 @@
 from os import path
 from numpy.core.defchararray import asarray
+from numpy.lib import twodim_base
 import filterCoords
 import numpy
 from numpy import save
 from numpy import load
+import numpy as np
 import sys
+from PIL import Image
 sys.setrecursionlimit(10**8)
 
 xyzFile = open('goeree.xyz','r')
@@ -20,15 +23,14 @@ widthx = TwoDArr.shape[1]-1
 class MauAlgorithm:
 
     def checkNode(self, y, x, nap):
-        if TwoDArr[y,x].height < nap :
+        if float(TwoDArr[y,x].height) < nap :
                 return(self.pathFinder(y,x,nap))
         else: 
             print("Path is above NAP, therefore safe")
 
     def pathFinder(self,y,x,nap):
-        if TwoDArr[y,x].height != -9999:
-            TwoDArr[y,x].RGB = "#FFB6C1"
-            print(str(TwoDArr[y,x].height) +" "+ str(TwoDArr[y,x].RGB) +" "+ str(TwoDArr[y,x].x) +" "+ str(TwoDArr[y,x].y))
+        TwoDArr[y,x].RGB = "#FFB6C1"
+        print(str(TwoDArr[y,x].height) +" "+ str(TwoDArr[y,x].RGB) +" "+ str(TwoDArr[y,x].x) +" "+ str(TwoDArr[y,x].y))
             
         if y>0 and TwoDArr[y-1,x].RGB != "#FFB6C1" and float(TwoDArr[y-1,x].height) < nap:
                 return self.pathFinder(y-1,x,nap)
@@ -50,14 +52,95 @@ class MauAlgorithm:
         if y<lengthy and x>0 and TwoDArr[y+1,x-1].RGB != "#FFB6C1" and float(TwoDArr[y+1,x-1].height) < nap:
             return self.pathFinder(y+1,x-1,nap)
 
+# class GetLine:
+#     def getLine(self,lengthy,widthx):
+#         X=0
+#         Y=0
+#         pointArray = []
+#         for _ in range(0,widthx+1):
+#             value = lengthy+2
+#             Y=0
+#             for _ in range(0,lengthy+1):
+#                 if TwoDArr[Y,X].height == -9999 and TwoDArr[Y,X].y < value:
+#                     value = TwoDArr[Y,X].y
+#                 Y+=1
+#             pointArray.append(value)
+#             X+=1
+            
+#         return pointArray
 
+class MapCreator:
+    def drawPath(self,start):
+        array = np.zeros([widthx+1, lengthy+1, 4], dtype=np.uint8)
+        array[:,:] = [248, 213, 104, 0] #Sandy backside
+        
+        x=0
+        y=0
+        for _ in range(0,widthx+1):
+            for _ in range(0,lengthy+1):
+                if nap - TwoDArr[y,x].height > 2 and TwoDArr[y,x].RGB=="#FFB6C1":
+                    TwoDArr[y,x].RGB="#FF0000"
+                elif nap - TwoDArr[y,x].height > 1 and TwoDArr[y,x].RGB=="#FFB6C1":
+                    TwoDArr[y,x].RGB="#FFA500"
+                elif nap - TwoDArr[y,x].height > 0 and TwoDArr[y,x].RGB=="#FFB6C1":
+                    TwoDArr[y,x].RGB="#FFFF00"
+                y+=1
+            x+=1
+        x=0
+        y=0
+        for _ in range(0,widthx+1):
+            for _ in range(0,lengthy+1):
+                if TwoDArr[y,x].RGB == "#FF0000":
+                    array[TwoDArr[y,x].x,TwoDArr[y,x].y] = [255, 0, 0,100] #red area
+                if TwoDArr[y,x].RGB == "#FFA500":
+                    array[TwoDArr[y,x].x,TwoDArr[y,x].y] = [255, 165, 0,100] #orange area
+                if TwoDArr[y,x].RGB == "#FFFF00":
+                    array[TwoDArr[y,x].x,TwoDArr[y,x].y] = [255, 255, 0,100] #yellow area
+                if TwoDArr[y,x].RGB == "#39ff14":
+                    array[TwoDArr[y,x].x,TwoDArr[y,x].y] = [57, 255, 20,100] #yellow area
+                y+=1
+            x+=1
+
+        # x=0
+        # for _ in range(0,widthx+1):
+        #     array[lengthy,x] = [255, 97, 71,100] #red starting point
+        #     x+=1
+        # img = Image.fromarray(array)
+        # img.save('testrgb.png')
+
+
+
+
+        # for _ in TwoDArr:
+        #     if obj[count]["RGB"] == "#0000FF":
+        #         array[obj[count]["X"],obj[count]["Y"]] = [55, 102, 246] #blue line
+        #     count+=1
+        # startpar=0
+        # for _ in range(0,start):
+        #     array[obj[startpar]["X"],obj[startpar]["Y"]] = [255, 97, 71] #red starting point
+        #     startpar+=1
+        # for _ in range(start,secondhalf):
+        #     array[obj[startpar]["X"],obj[startpar]["Y"]] = [255, 97, 71] #red starting point
+        #     startpar+=1
+        # img = Image.fromarray(array)
+        # img.save('testrgb.png')
+
+
+
+
+
+
+# dothing = GetLine()
+
+# startline = dothing.getLine(114,424)
 
 find = MauAlgorithm()
 nap=4
 count=0
-for _ in range(0,widthx):
+for _ in range(0,widthx+1):
     find.checkNode(0,count,nap)
     count+=1
+
 
 # print(TwoDArr[lengthy-1,0].x)
 # TwoDArr2 = asarray(TwoDArr)
