8fe3357ae227d5b1cacec2a94a2ffce11800eefc
Kasper <vdpolkasper@gmail.com>
2021-06-07 19:48:54 +0200

    Overbodige functies verwijderd

--- backend/filterCoords.py
+++ backend/filterCoords.py
@@ -40,6 +40,13 @@ class Edge:
 	def GetYForX(self, x):
 		diffX = x - self.p1.x
 		moveY = diffX * self.CalculateSlope()
+		print()
+		print(self.p1.x)
+		print(x)
+		print("difference X: " + str(diffX) + " = " + str(x)+ " - " + str(self.p1.x))
+		print("Move Y: " + str(moveY) + " = "+ str(diffX) + " * " + str(self.slope))
+		print("Y value for X: " + str(self.p1.y+moveY))
+		print()
 		return self.p1.y + moveY
 
 	def GetXForY(self, y):
@@ -52,18 +59,6 @@ class Edge:
 	# ONLY WORKS FROM THE BOTTOM EDGE
 	# GIVE LEFT EDGE AS INPUT
 	def IntersectionPoint(self, point, edge):
-		# # calculate new line slope, x 0 value
-		# newLineSlope = slope
-		# diffX = 0 - point.x
-		# moveY = diffX * newLineSlope
-		# newLineYForZeroX = point.y + moveY
-		# tempEdge = Edge(point, Point(0, newLineYForZeroX))
-		# thisLineYForZeroX = self.GetYForX(0)
-		# print(str(newLineYForZeroX), " + ", str(newLineSlope), "x = ", str(thisLineYForZeroX), " + ", str(self.CalculateSlope()), "x")
-		# modifier = newLineYForZeroX - thisLineYForZeroX
-		# slope = self.CalculateSlope() - tempEdge.CalculateSlope()
-		# intersectionPointY = modifier / slope
-		# print(intersectionPointY)
 		# return Point(0, intersectionPointY)
 		xModifier = point.x - edge.p1.x  
 		xValue = xModifier * (1 + abs(self.slope))
@@ -90,9 +85,6 @@ class QuadrilateralFilter:
 	# Takes point as input, returns True if it appears in this rectangle
 	def withinQuadrilateral(self, p):
 		# check = False
-		# if p.y > 427280 and p.y < 427698:
-		# 	print(p.y)
-		# 	check = True
 		if self.topEdge.GetYForX(p.x) < p.y:
 			# if check:
 			# 	print(self.topEdge.GetYForX(p.x), p.y)
@@ -109,11 +101,6 @@ class QuadrilateralFilter:
 			return False
 		print('true')
 		return True
-
-	# Gets width and height of a numpy array
-	def getProportions(self):
-		values = self.getMinMaxValues()
-		return [int(values[0] - values[1]), int(values[2] - values[3])]
 
 	# returns minimum and maximum x and y values
 	def getMinMaxValues(self):
@@ -140,10 +127,10 @@ class QuadrilateralFilter:
 		return [rightXValue, leftXValue, topYValue, botYValue]
 
 class TwoDimensionalXYZArrayStraight:
-	def __init__(self, QFilter, xyzFile):
+	def __init__(self, QFilter):
 		self.QFilter = QFilter
 		self.arr = self.createArray()
-		self.fillArray(xyzFile)
+		#self.fillArray(xyzFile)
 
 	# Creates an empty numpy 2D array with the length and width of the edges	
 	def createArray(self):
@@ -154,28 +141,32 @@ class TwoDimensionalXYZArrayStraight:
 			}
 		return numpy.empty((int(proportions["x"]), int(proportions["y"])), dtype=float)
 
-	# Adds a height value into the array on a given x and y index
-	def addValue(self, x, y, val):
-		self.arr[x,y] = val
-		return
-
 	# Fills the array with the values inside of the .xyz file
 	def fillArray(self, xyzFile):
-		# Implement xyzFile class here
-		pointList = XYZFileHandler(xyzFile).getPointList()
 		# calculate length from given point in pointlist to its edge
 		offsetX = self.QFilter.left2.x
 		offsetY = self.QFilter.left2.y
 		print("OffsetX: ", str(offsetX))
 		print("OffsetY: ", str(offsetY))
-		for point in pointList:
-			if self.QFilter.withinQuadrilateral(point):
-				print("passed test")
-				coords = self.QFilter.bottomEdge.IntersectionPoint(point, self.QFilter.leftEdge)
-				if self.arr[int(round(coords[0])-offsetX), int(round(coords[1])-offsetY)] != None:
-					input("collision found")
-				self.arr[int(round(coords[0])-offsetX), int(round(coords[1])-offsetY)] = point.height
-				print(point.height)
+		xyzFileHandle = open(xyzFile, 'r')
+		# Implement xyzFile class here
+		i = 0
+		for line in xyzFileHandle:
+			for val in line.strip().split(" "):
+				i+= 1
+				if i % 3 == 1:
+					x = float(val)
+				if i % 3 == 2:
+					y = float(val)
+				if i % 3 == 0:
+					point = Point(x,y,val)
+					if self.QFilter.withinQuadrilateral(point):
+						print("passed test")
+						coords = self.QFilter.bottomEdge.IntersectionPoint(point, self.QFilter.leftEdge)
+						if self.arr[int(round(coords[0])-offsetX), int(round(coords[1])-offsetY)] != None:
+							input("collision found")
+						self.arr[int(round(coords[0])-offsetX), int(round(coords[1])-offsetY)] = point.height
+						print(point.height)
 		print("Filled array succesfully")
 		print("Writing text file..")
 		numpy.savetxt("arr.csv", self.arr, delimiter=",")
@@ -201,103 +192,3 @@ class XYZFileHandler:
 					pointList.append(tempPoint)
 		print(pointList[0].x, pointList[0].y)
 		return pointList
-
-
-# x and y is the array position, realx and realy are the actual coordinates from the xyz file
-class MauricePoint:
-	def __init__(self, height, realx, realy, x, y, RGB):
-		self.height = height
-		self.RGB = RGB
-		self.x = x
-		self.y = y
-		self.point = Point(realx, realy)
-
-class TwoDimensionalXYZArray:
-	def __init__(self, QFilter):
-		self.QFilter = QFilter
-		self.arr = self.createArray()
-
-	def createArray(self):
-		proportions = self.QFilter.getProportions()
-		return numpy.empty((proportions[0], proportions[1]), dtype=MauricePoint)
-
-	def coordinateToIndex(self, point):
-		minMaxValues = self.QFilter.getMinMaxValues()
-		coordinates = {
-			"x" : int(point.x - minMaxValues[1]),
-			"y" : int(point.y - minMaxValues[3])
-		}
-		return coordinates
-
-	def addPoint(self, mauricePoint):
-		self.arr[mauricePoint.x, mauricePoint.y] = mauricePoint
-		return
-
-	def fillEmptyArrayPoints(self):
-		for x in range(0, self.arr.shape[0]):
-			for y in range(0, self.arr.shape[1]):
-				if self.arr[x,y] == None:
-					self.arr[x,y] = MauricePoint(-9999, 0, 0, x, y, '#FFFFFF')
-
-
-def RunFilterOutput2DArray(xyzFile, workingFolder, left1x, left1y, left2x, left2y, right1x, right1y, right2x, right2y):
-	filePath = workingFolder + xyzFile
-	xyzFileHandle = open('goeree.xyz', 'r')
-
-	left1 = Point(left1x, left1y)
-	left2 = Point(left2x, left2y)
-	right1 = Point(right1x, right1y)
-	right2 = Point(right2x, right2y)
-
-	qFilter = QuadrilateralFilter(left1, left2, right1, right2)
-	output2DArray = TwoDimensionalXYZArray(qFilter)
-
-	i = 0
-	for line in xyzFileHandle:
-		for val in line.strip().split(" "):
-			i+= 1
-			if i % 3 == 1:
-				x = float(val)
-			if i % 3 == 2:
-				y = float(val)
-			if i % 3 == 0:
-				tempPoint = Point(x,y)
-				if qFilter.withinQuadrilateral(tempPoint):
-					indexValues = output2DArray.coordinateToIndex(tempPoint)
-					tempMauricePoint = MauricePoint(val, x, y, indexValues["x"], indexValues["y"], "#000000")
-					output2DArray.addPoint(tempMauricePoint)
-	output2DArray.fillEmptyArrayPoints()
-	return output2DArray.arr
-
-
-# Here we parse through each line of the original XYZ file, test if its point is in the quadrilateral, and if it does we add it to the new xyz file
-def RunFilter(oldFile, left1x, left1y, right1x, right1y, right2x, right2y, left2x, left2y):
-	originalXyzFile = oldFile
-	newXyzFile = "new" + oldFile[:-3] + "csv"
-
-	fhandle = open(originalXyzFile, 'r')
-	filteredXyz = open(newXyzFile, 'w')
-	filteredXyzWriter = csv.writer(filteredXyz)
-
-
-	left1 = Point(left1x, left1y)
-	left2 = Point(left2x, left2y)
-	right1 = Point(right1x, right1y)
-	right2 = Point(right2x, right2y)
-
-	qFilter = QuadrilateralFilter(left1, left2, right1, right2)
-
-	i = 0
-	for line in fhandle:
-		for val in line.strip().split(" "):
-			i+= 1
-			if i % 3 == 1:
-				x = float(val)
-			if i % 3 == 2:
-				y = float(val)
-			if i % 3 == 0:
-				tempPoint = Point(x,y)
-				if qFilter.withinQuadrilateral(tempPoint):
-#                    filteredXyz.write(str(tempPoint.x) + " " + str(tempPoint.y) + " " + str(val) + "\n")
-					filteredXyzWriter.writerow([str(tempPoint.x), str(tempPoint.y), str(val)])
-	return newXyzFile
