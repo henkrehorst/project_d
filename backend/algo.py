ea65136d846017314021805014ad159dcd03a3c6
markhof010 <45596281+markhof010@users.noreply.github.com>
2021-06-08 21:56:59 +0200

    algo

--- backend/algo.py
+++ backend/algo.py
@@ -1,62 +1,91 @@
 from GetPoints import straightSquare
 from numpy.core.records import array
-import filterCoords as fc
+from PIL import Image
+import numpy as np
 
 def heightNumber(height, waterLevel):
-    return 0
+    niveau = waterLevel // 3
+
+    #check if point exists
+    if height == -9999:
+        return 0
+    #check if point is very low beneath the waterlevel
+    elif height <= niveau:
+        return 3
+    #check if point is medium low beneath the waterlevel
+    elif height > niveau and height <= (niveau*2):
+        return 2
+    #check if point is net very low beneath the waterlevel
+    elif height > (niveau*2) and height <= waterLevel:
+        return 1
+    #height is above waterlevel
+    else:
+        return 0
 
 
 def algorithm(arr, waterLevel, startingPoint):
-    
-    imgArray = [[0 for x in range(len(arr))] for y in range(len(arr[0]))]
-    ToCheck = []
 
-    startX = startingPoint.x - arr[0][0].x
-    startY = arr[0][0].y - startingPoint.y
+    imgArray = [[-1 for x in range(len(arr))] for y in range(len(arr[0]))]
+    ToCheck = []
+    sp = [startingPoint["x"],startingPoint["y"]]
     
-    ToCheck.append([startX,startY])
+    ToCheck.append(sp)
 
     while len(ToCheck) > 0:
         x = ToCheck[0][0]
         y = ToCheck[0][1]
         
         #check left
-        if x - 1 > 0:
-            if filter.withinQuadrilateral(arr[y][x-1]):
-                imgArray[y][x-1] = heightNumber(arr[y][x-1].height, waterLevel)
+        if x - 1 > 0 and imgArray[y][x-1] != -1:
+            level = heightNumber(arr[x-1][y], waterLevel)
+            imgArray[y][x-1] = level
+            if level != 0:
+                ToCheck.append([x-1,y])
 
         #check right
-        if x + 1 < len(arr[y]):
-            if filter.withinQuadrilateral(arr[y][x+1]):
-                imgArray[y][x+1] = heightNumber(arr[y][x-1].height, waterLevel)
-        
-        #check under
-        if y + 1 < len(arr):
-            if filter.withinQuadrilateral(arr[y+1][x]):
-                imgArray[y+1][x] = heightNumber(arr[y][x-1].height, waterLevel)
-        
+        if x + 1 < len(arr) and imgArray[y][x+1] != -1:
+            level = heightNumber(arr[x+1][y], waterLevel)
+            imgArray[y][x+1] = level
+            if level != 0:
+                ToCheck.append([x+1,y])
+            
+            #check under
+        if y - 1 > 0 and imgArray[y-1][x] != -1:
+            level = heightNumber(arr[x][y-1], waterLevel)
+            imgArray[y-1][x] = level
+            if level != 0:
+                ToCheck.append([y-1][x-1])
+            
         #check above
-        if y - 1 > 0:
-            arr[y-1][x]
+        if y + 1 < len(arr[x]) and imgArray[y+1][x] != -1:
+            level = heightNumber(arr[x][y+1], waterLevel)
+            imgArray[y+1][x] = level
+            if level != 0:
+                ToCheck.append([x,y+1])
+        
+        ToCheck.remove(ToCheck[0])
+    return imgArray
 
 
+def makeImage(Array):
+    # Make the array needed to convert it later to a image
+    arr = np.zeros([len(Array),len(Array[0]), 4], dtype=np.uint8)
+    i = 0
+    # This will give the right values within the array
+    while i < len(Array):
+        j = 0
+        while j < len(Array[i]):
+            if Array[i][j] == 0 or Array[i][j] == -1:
+                arr[i][j] = [255, 255, 255, 0]
+            if Array[i][j] == 1:
+                arr[i,j] = [255, 255, 0,100] # Yellow
+            if Array[i][j] == 2:
+                arr[i,j] =  [255, 165, 0,100] # Orange
+            if Array[i][j] == 3:
+                arr[i,j] = [255, 0, 0,100] # Red
+            j = j+1
+        i = i+1
 
-
-
-    # i = 0
-
-    # while i < len(arr):
-    #     j = 0
-    #     while j < len(arr[i]):
-    #         x = arr[i][j].x
-    #         y = arr[i][j].y
-    #         z = arr[i][j].z
-    #         point = fc.Point(x,y,z)
-    #         if filter.withinQuadrilateral(point):
-    #             measurement = waterLevel // 3
-    #             if z <= measurement:
-    #                 imgArray[i][j] = 3
-    #             elif z > measurement and z <= (measurement * 2):
-    #                 imgArray[i][j] = 2
-    #             elif z > (measurement * 2) and z < waterLevel:
-    #                 imgArray[i][j] = 1
+    # Convert the array to image and save it
+    image = Image.fromarray(arr)
+    image.save('test.png', 'PNG', transparency=0)
--- backend/testScript.py
+++ backend/testScript.py
@@ -1,4 +1,5 @@
 import filterCoords as fc
+import algo
 
 l2 = fc.Point(52590.5, 427698.9114832536)
 l1 = fc.Point(52630.5, 427706.0885167464)
@@ -8,4 +9,9 @@ r1 = fc.Point(52705.5, 427288.0885167464)
 startingPoints = (fc.Point(52610.5, 427702.5), fc.Point(52685.5, 427284.5))
 
 qF = fc.QuadrilateralFilter(l1, l2, r1, r2)
-fc.RunFilterOutput2DArray('goeree.xyz', './', 52630.5, 427706.0885167464,52590.5, 427698.9114832536, 52705.5, 427288.0885167464,52665.5, 427280.9114832536, startingPoints)
+tp = fc.RunFilterOutput2DArray('goeree.xyz', './', 52630.5, 427706.0885167464,52590.5, 427698.9114832536, 52705.5, 427288.0885167464,52665.5, 427280.9114832536, startingPoints)
+
+print("starting algo")
+imgArr = algo.algorithm(tp[0], 8, tp[1][0])
+
+algo.makeImage(imgArr)
